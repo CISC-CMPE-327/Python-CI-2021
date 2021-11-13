200b8658939f24221b9375917021882b8949f44e
Steven <ding@cs.queensu.ca>
2021-11-13 01:36:29 -0500

    delpy cli option

--- qbay/__init__.py
+++ qbay/__init__.py
@@ -2,6 +2,11 @@
 an init file is required for this folder to be considered as a module
 '''
 from flask import Flask
+import os
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite'
+db_string = os.getenv('db_string')
+if db_string:
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_string
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
