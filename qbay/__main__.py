2511674039d85b8627cb5cf16ac2c821bd255612
steven <ding@cs.queensu.ca>
2022-10-06 02:57:00 -0400

    fix context & add exit

--- qbay/__main__.py
+++ qbay/__main__.py
@@ -5,7 +5,7 @@ from qbay.cli import login_page, regsiter_page
 def main():
     while True:
         selection = input(
-            'Welcome. Please type 1 to login. Or type 2 register.')
+            'Welcome. Please type 1 to login. Or type 2 register. Or type 3 to exit')
         selection = selection.strip()
         if selection == '1':
             user = login_page()
@@ -16,6 +16,8 @@ def main():
                 print('login failed')
         elif selection == '2':
             regsiter_page()
+        elif selection == '3':
+            break
 
 
 if __name__ == '__main__':
