a43bee900efce2fd79b6a775bbf08f1f57e78e9c
steven <ding@cs.queensu.ca>
2022-10-06 02:59:50 -0400

    pep8 error

--- qbay/__main__.py
+++ qbay/__main__.py
@@ -5,7 +5,8 @@ from qbay.cli import login_page, regsiter_page
 def main():
     while True:
         selection = input(
-            'Welcome. Please type 1 to login. Or type 2 register. Or type 3 to exit')
+            'Welcome. Please type 1 to login. '
+            'Or type 2 register. Or type 3 to exit')
         selection = selection.strip()
         if selection == '1':
             user = login_page()
